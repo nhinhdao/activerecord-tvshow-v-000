d3ce5486726255b71d7e1ce6dffa53d7bbc9d655
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-08-01 14:52:31 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,28 @@ class Show < ActiveRecord::Base
     def self.highest_rating
         Show.maximum(:rating)
     end
+
+    def Show::most_popular_show
+        Show.find_by(highest_rating)
+    end
+
+    def Show::lowest_rating
+        Show.minimum(:rating)
+    end
+
+    def Show::least_popular_show
+        Show.find_by(lowest_rating)
+    end
+
+    def Show::ratings_sum
+        Show.sum(:rating)
+    end
+
+    def Show::popular_shows
+        Show.where('rating > 5')
+    end
+
+    def Show::shows_by_alphabetical_order
+        Show.order(name: :desc)
+    end
 end
